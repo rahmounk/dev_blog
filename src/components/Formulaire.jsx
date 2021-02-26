af1bb30cc5e0c4f9feebda7b444768ccbd445e71
kr <rahmoun.karim5@gmail.com>
2021-02-26 16:24:33 +0100

    commit avec rajout de axios

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,59 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Formulaire = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmit(true);
-    setPost(title, content, tags);
-    console.log(post);
-  };
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [tags, setTags] = useState([]);
+    const [submit, setSubmit] = useState(false);
+    const [post, setPost] = useState({});
 
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [tags, setTags] = useState([]);
-  const [submit, setSubmit] = useState(false);
-  const [post, setPost] = useState({});
+    function uploadData() {
+        const postUrl = "http://localhost:5000/api/v1/posts";
+        axios
+            .post(postUrl, post)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error.reponse.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            });
+    }
 
+    useEffect(() => {
+        console.log(post);
+        uploadData();
+    }, [post]);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmit(true);
+        setPost({
+            ...{ title },
+            ...{ content },
+            ...{ tags },
+        });
+    };
 
-  return (
-    <div>
-      <form className="post_form" onSubmit={handleSubmit}>
-        <label htmlFor="post_title">Titre du post : </label>
-        <input
-          type="text"
-          name="post_title"
-          id="post_title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    return (
+        <div>
+            <form className="post_form" onSubmit={handleSubmit}>
+                <label htmlFor="post_title">Titre du post : </label>
+                <input
+                    type="text"
+                    name="post_title"
+                    id="post_title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
 
-        <label htmlFor="post_content">Contenu : </label>
-        <textarea
-          placeholder="Quoi de neuf ?"
-          name="post_content"
-          id="post_content"
-          cols="30"
-          rows="10"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
+                <label htmlFor="post_content">Contenu : </label>
+                <textarea
+                    placeholder="Quoi de neuf ?"
+                    name="post_content"
+                    id="post_content"
+                    cols="30"
+                    rows="10"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
 
-        <label htmlFor="post_tags">Tags/Catégories #</label>
-        <input
-          type="text"
-          name="post_tags"
-          id="post_tags"
-          value={tags}
-          onChange={(e) => {
-            let newTags = e.target.value.split(",");
-            setTags(newTags);
-          }}
-        />
-        <input className="button" type="submit" value="CRÉER MON POST" />
-      </form>
-    </div>
-  );
+                <label htmlFor="post_tags">Tags/Catégories #</label>
+                <input
+                    type="text"
+                    name="post_tags"
+                    id="post_tags"
+                    value={tags}
+                    onChange={(e) => {
+                        let newTags = e.target.value.split(",");
+                        setTags(newTags);
+                    }}
+                />
+                <input className="button" type="submit" value="CRÉER MON POST" />
+            </form>
+        </div>
+    );
 };
 
 export default Formulaire;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "../styles/components/_Blog.scss";
-
 import Formulaire from "../components/Formulaire";
 import Post from "../components/Post";
+import axios from "axios";
 
-const url = "http://localhost:3001/api/v1/posts";
+const url = "http://localhost:5000/api/v1/posts";
 
 const Blog = () => {
-  const [api, setApi] = useState([]);
-  const fetchApi = async () => {
-    try {
-      const response = await fetch(url);
-      const api = await response.json();
-      setApi(api);
-      console.log(api);
-    } catch (error) {}
-  };
+    const [api, setApi] = useState([]);
+    const fetchApi = async () => {
+        try {
+            const response = await axios.get(url);
+            setApi(response.data);
+            console.log(api);
+        } catch (error) {}
+    };
 
-  useEffect(() => {
-    fetchApi();
-  }, []);
+    useEffect(() => {
+        fetchApi();
+    }, []);
 
-  return (
-    <div className="blog">
-      <h2>Blog</h2>
-      <Formulaire />
-      <Post api={api} />
-    </div>
-  );
+    return (
+        <div className="blog">
+            <h2>Blog</h2>
+            <Formulaire />
+            <Post api={api} />
+        </div>
+    );
 };
 export default Blog;
